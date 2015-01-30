15aa009f261ed3490cf053e9879262eb91473909
rriva002 <rriva002@ucr.edu>
2015-01-29 21:22:00 -0800

    Added comments and new features to DrawReader
    You can now remove platforms and trampolines by right-clicking on them,
    and you can't draw an object that intersects the player. The code has
    also been cleaned up slightly.

--- seniorDesign/Assets/Scripts/DrawReader.cs
+++ seniorDesign/Assets/Scripts/DrawReader.cs
@@ -9,8 +9,8 @@ public class DrawReader : MonoBehaviour
 	private List<Vector3> points;
 	private List<GameObject> platforms;
 	private List<GameObject> trampolines;
-	private bool[][] platformTemplate;
-	private bool[][] trampolineTemplate;
+	private bool[,] platformTemplate;
+	private bool[,] trampolineTemplate;
 	private float platformScoreThreshold = 5f;
 	private float trampolineScoreThreshold = 6f;
 	private float minX;
@@ -18,12 +18,16 @@ public class DrawReader : MonoBehaviour
 	private float minY;
 	private float maxY;
 	private float distance;
+	private int gridSize = 5;
+	private int halfGridSize;
 	public float maxDistance;
 	public int maxPlatforms;
 	public int maxTrampolines;
+	public GameObject player;
 
 	void Awake()
 	{
+		halfGridSize = gridSize / 2;
 		points = new List<Vector3>();
 		platforms = new List<GameObject>();
 		trampolines = new List<GameObject>();
@@ -43,6 +47,7 @@ public class DrawReader : MonoBehaviour
 	{
 		if(Input.GetMouseButtonDown(0))
 		{
+			//Set initial min/max drawing point values.
 			minX = float.PositiveInfinity;
 			maxX = float.NegativeInfinity;
 			minY = float.PositiveInfinity;
@@ -54,21 +59,40 @@ public class DrawReader : MonoBehaviour
 		}
 		else if(Input.GetMouseButtonUp(0))
 		{
-			isMousePressed = false;
-
+			//Create an object if it was drawn correctly and remove the drawing.
 			interpretDrawing();
 			drawing.SetVertexCount(0);
 			points.RemoveRange(0, points.Count);
+
+			isMousePressed = false;
+		}
+		else if(Input.GetMouseButtonUp(1))
+		{
+			//Cast a ray from the mouse position to determine which object was right-clicked.
+			Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			//If the ray hit something, delete it.
+			if(Physics.Raycast(camRay, out hit) && hit.collider != null)
+			{
+				if(removeClicked(hit, platforms) || removeClicked(hit, trampolines))
+				{
+					//Display error message?
+				}
+			}
 		}
 
+		//Add points to the drawing while the left mouse button is down.
 		if(isMousePressed && distance < maxDistance)
 		{
+			//Get a point from the current mouse position. The camera should be at position (0, 1, -10) and rotation (0, 0, 0).
 			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
 			mousePos.z = 0f;
 
 			if(!points.Contains(mousePos)) 
 			{
+				//Calculate the distance drawn.
 				if(points.Count > 0)
 				{
 					Vector3 lastPoint = points[points.Count - 1];
@@ -76,10 +100,12 @@ public class DrawReader : MonoBehaviour
 					distance += Mathf.Sqrt(Mathf.Pow(mousePos.x - lastPoint.x, 2f) + Mathf.Pow(mousePos.y - lastPoint.y, 2f));
 				}
 
+				//Add point to the drawing.
 				points.Add(mousePos);
 				drawing.SetVertexCount(points.Count);
 				drawing.SetPosition(points.Count - 1, points[points.Count - 1]);
 
+				//Recalculate minimum and maximum point values.
 				minX = mousePos.x < minX ? mousePos.x : minX;
 				maxX = mousePos.x > maxX ? mousePos.x : maxX;
 				minY = mousePos.y < minY ? mousePos.y : minY;
@@ -88,6 +114,25 @@ public class DrawReader : MonoBehaviour
 		}
 	}
 
+	//Removes a drawn object when right-clicked.
+	private bool removeClicked(RaycastHit hit, List<GameObject> list)
+	{
+		//Checks each game object in the list for the object that was right-clicked.
+		foreach(GameObject gameObject in list)
+		{
+			if(hit.collider.Equals(gameObject.collider))
+			{
+				//Removes and destroys the right-clicked object.
+				list.Remove(gameObject);
+				Destroy(gameObject);
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	//Compares a drawing to object templates and creates an object if a match is found.
 	private void interpretDrawing()
 	{
 		float xLength = maxX - minX;
@@ -95,26 +140,29 @@ public class DrawReader : MonoBehaviour
 		bool horizontal = xLength > yLength;
 		float distanceFromMidpoint = (horizontal ? xLength : yLength) / 2f;
 		Vector2 midpoint = new Vector2((maxX + minX) / 2f, (maxY + minY) / 2f);
-		bool[][] grid = setUpGrid();
+		bool[,] grid = setUpGrid();
 
+		//Converts the drawing to grid format for comparison.
 		foreach(Vector3 point in points)
 		{
-			int x = 2 + (int) (2f * (point.x - midpoint.x) / distanceFromMidpoint);
-			int y = 2 + (int) (2f * (point.y - midpoint.y) / distanceFromMidpoint);
+			int x = halfGridSize + (int) (((float) halfGridSize) * (point.x - midpoint.x) / distanceFromMidpoint);
+			int y = halfGridSize + (int) (((float) halfGridSize) * (point.y - midpoint.y) / distanceFromMidpoint);
 
-			if(x < 0 || y < 0 || x > 4 || y > 4)
+			if(x < 0 || y < 0 || x > gridSize - 1 || y > gridSize - 1)
 			{
 				return;
 			}
 
-			grid[x][y] = true;
+			grid[x, y] = true;
 		}
 
+		//Calculates matching scores for each template.
 		float[] scores = {compareGrids(grid, platformTemplate) / platformScoreThreshold,
 						  compareGrids(grid, trampolineTemplate) / trampolineScoreThreshold};
 		int max = -1;
 		float maxScore = 0.8f;
 
+		//Finds the object template with the highest score (initially set high to avoid false positives).
 		for(int i = 0; i < scores.Length; i++)
 		{
 			if(scores[i] >= maxScore)
@@ -126,6 +174,7 @@ public class DrawReader : MonoBehaviour
 
 		print("Platform: " + scores[0] + " Trampoline: " + scores[1]);
 
+		//Create the object with the highest score.
 		switch(max)
 		{
 			case 0:
@@ -139,88 +188,118 @@ public class DrawReader : MonoBehaviour
 		}
 	}
 
-	private int compareGrids(bool[][] a, bool[][] b)
+	//Compares two grids and returns a score based on their similarity.
+	private int compareGrids(bool[,] a, bool[,] b)
 	{
 		int score = 0;
 
-		for(int i = 0; i < 5; i++)
+		for(int i = 0; i < gridSize; i++)
 		{
-			for(int j = 0; j < 5; j++)
+			for(int j = 0; j < gridSize; j++)
 			{
-				score += a[i][j] && b[i][j] ? 1 : 0;
+				score += a[i, j] && b[i, j] ? 1 : 0;
 			}
 		}
 
 		return score;
 	}
 
+	//Creates a platform at the specified position.
 	private void createPlatform(Vector2 position)
 	{
 		GameObject platform = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
+		//Sets platform properties.
 		platform.name = "Platform";
 		platform.transform.position = new Vector3(position.x, position.y, 0f);
 		platform.transform.localScale = new Vector3(maxX - minX, 1f, 1f);
-		
-		platforms.Add(platform);
-		
-		if(platforms.Count > maxPlatforms)
-		{
-			GameObject oldPlatform = platforms[0];
-			
-			platforms.Remove(oldPlatform);
-			Destroy(oldPlatform);
-		}
+
+		addObject(platform, platforms, maxPlatforms);
 	}
 
+	//Creates a trampoline at the speficied position.
 	private void createTrampoline(Vector2 position)
 	{
 		GameObject trampoline = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
 
+		//Sets trampoline properties.
 		trampoline.name = "Trampoline";
 		trampoline.transform.position = new Vector3(position.x, position.y, 0f);
-		trampoline.transform.localScale = new Vector3(maxX - minX, 0.1f, 1f);
+		trampoline.transform.localScale = new Vector3(maxX - minX, 0.1f, maxX - minX);
 
-		trampolines.Add(trampoline);
+		addObject(trampoline, trampolines, maxTrampolines);
+	}
 
-		if(trampolines.Count > maxTrampolines)
+	//Adds a drawn object to the specified list.
+	private void addObject(GameObject gameObject, List<GameObject> list, int max)
+	{
+		//The object isn't added if it intersects the player.
+		if(!intersectsPlayer(gameObject))
 		{
-			GameObject oldTrampoline = trampolines[0];
+			//Adds the object to the list.
+			list.Add(gameObject);
 
-			trampolines.Remove(oldTrampoline);
-			Destroy(oldTrampoline);
+			//Deletes the oldest object in the list if the list contains too many objects.
+			if(list.Count > max)
+			{
+				GameObject oldGameObject = list[0];
+				
+				list.Remove(oldGameObject);
+				Destroy(oldGameObject);
+			}
 		}
 	}
 
+	//Determines whether a drawn object intersects the player.
+	private bool intersectsPlayer(GameObject gameObject)
+	{
+		//Destroys the object if it intersects the player.
+		if(gameObject.collider.bounds.Intersects(player.collider.bounds))
+		{
+			Destroy(gameObject);
+			return true;
+		}
+
+		return false;
+	}
+
+	//Sets up the grid template for a platform.
 	private void setUpPlatformTemplate()
 	{
 		platformTemplate = setUpGrid();
 
-		for(int i = 0; i < 5; i++)
+		//Creates a horizontal line in the grid.
+		for(int i = 0; i < gridSize; i++)
 		{
-			platformTemplate[i][2] = true;
+			platformTemplate[i, halfGridSize] = true;
 		}
 	}
 
+	//Sets up the grid template for a trampoline.
 	private void setUpTrampolineTemplate()
 	{
 		trampolineTemplate = setUpGrid();
 
-		for(float i = 0f; i < 25f; i++)
+		float intervals = (float) (gridSize * gridSize);
+
+		//Creates a circle in the grid.
+		for(float i = 0f; i < intervals; i++)
 		{
-			trampolineTemplate[2 + (int) (2f * Mathf.Cos(i * 2f * Mathf.PI / 25f))][2 + (int) (2f * Mathf.Sin(i * 2f * Mathf.PI / 25f))] = true;
+			trampolineTemplate[halfGridSize + (int) (((float) halfGridSize) * Mathf.Cos(i * 2f * Mathf.PI / intervals)),
+				halfGridSize + (int) (((float) halfGridSize) * Mathf.Sin(i * 2f * Mathf.PI / intervals))] = true;
 		}
 	}
 
-	private bool[][] setUpGrid()
+	//Creates a "blank" grid template.
+	private bool[,] setUpGrid()
 	{
-		bool[][] grid = {new bool[5], new bool[5], new bool[5], new bool[5], new bool[5]};
+		bool[,] grid = new bool[gridSize, gridSize];
 
-		foreach(bool[] row in grid)
+		for(int i = 0; i < gridSize; i++)
 		{
-			for(int i = 0; i < row.Length; i++)
+			for(int j = 0; j < gridSize; j++)
 			{
-				row[i] = false;
+				grid[i, j] = false;
 			}
 		}
 
