608f9f79384b57780035fd1e3d6e4cda85df3909
twang033 <tina.wang365@gmail.com>
2015-02-03 17:24:07 -0800

    player can respawn

--- seniorDesign/Assets/Scripts/DrawReader.cs
+++ seniorDesign/Assets/Scripts/DrawReader.cs
@@ -21,7 +21,7 @@ public class DrawReader : MonoBehaviour
 	public float maxDistance;
 	public int maxPlatforms;
 	public int maxTrampolines;
-
+	
 	void Awake()
 	{
 		points = new List<Vector3>();
@@ -31,14 +31,14 @@ public class DrawReader : MonoBehaviour
 		drawing = gameObject.AddComponent<LineRenderer>();
 		//line.material = new Material(Shader.Find("Particles/Additive"));
 		drawing.useWorldSpace = true;
-
+		
 		setUpPlatformTemplate();
 		setUpTrampolineTemplate();
 		drawing.SetVertexCount(0);
 		drawing.SetWidth(0.1f, 0.1f);
 		//line.SetColors(Color.green, Color.green);
 	}
-
+	
 	void Update() 
 	{
 		if(Input.GetMouseButtonDown(0))
@@ -49,37 +49,38 @@ public class DrawReader : MonoBehaviour
 			maxY = float.NegativeInfinity;
 			isMousePressed = true;
 			distance = 0f;
-
+			
 			//line.SetColors(Color.green, Color.green);
 		}
 		else if(Input.GetMouseButtonUp(0))
 		{
 			isMousePressed = false;
-
+			
 			interpretDrawing();
 			drawing.SetVertexCount(0);
 			points.RemoveRange(0, points.Count);
 		}
-
+		
 		if(isMousePressed && distance < maxDistance)
 		{
+			//The line below assumes that the camera is pointing in the default direction
 			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
+			
 			mousePos.z = 0f;
-
+			
 			if(!points.Contains(mousePos)) 
 			{
 				if(points.Count > 0)
 				{
 					Vector3 lastPoint = points[points.Count - 1];
-
+					
 					distance += Mathf.Sqrt(Mathf.Pow(mousePos.x - lastPoint.x, 2f) + Mathf.Pow(mousePos.y - lastPoint.y, 2f));
 				}
-
+				
 				points.Add(mousePos);
 				drawing.SetVertexCount(points.Count);
 				drawing.SetPosition(points.Count - 1, points[points.Count - 1]);
-
+				
 				minX = mousePos.x < minX ? mousePos.x : minX;
 				maxX = mousePos.x > maxX ? mousePos.x : maxX;
 				minY = mousePos.y < minY ? mousePos.y : minY;
@@ -87,7 +88,7 @@ public class DrawReader : MonoBehaviour
 			}
 		}
 	}
-
+	
 	private void interpretDrawing()
 	{
 		float xLength = maxX - minX;
@@ -96,25 +97,25 @@ public class DrawReader : MonoBehaviour
 		float distanceFromMidpoint = (horizontal ? xLength : yLength) / 2f;
 		Vector2 midpoint = new Vector2((maxX + minX) / 2f, (maxY + minY) / 2f);
 		bool[][] grid = setUpGrid();
-
+		
 		foreach(Vector3 point in points)
 		{
 			int x = 2 + (int) (2f * (point.x - midpoint.x) / distanceFromMidpoint);
 			int y = 2 + (int) (2f * (point.y - midpoint.y) / distanceFromMidpoint);
-
+			
 			if(x < 0 || y < 0 || x > 4 || y > 4)
 			{
 				return;
 			}
-
+			
 			grid[x][y] = true;
 		}
-
+		
 		float[] scores = {compareGrids(grid, platformTemplate) / platformScoreThreshold,
-						  compareGrids(grid, trampolineTemplate) / trampolineScoreThreshold};
+			compareGrids(grid, trampolineTemplate) / trampolineScoreThreshold};
 		int max = -1;
 		float maxScore = 0.8f;
-
+		
 		for(int i = 0; i < scores.Length; i++)
 		{
 			if(scores[i] >= maxScore)
@@ -123,26 +124,26 @@ public class DrawReader : MonoBehaviour
 				max = i;
 			}
 		}
-
+		
 		print("Platform: " + scores[0] + " Trampoline: " + scores[1]);
-
+		
 		switch(max)
 		{
-			case 0:
-				createPlatform(midpoint);
-				break;
-			case 1:
-				createTrampoline(midpoint);
-				break;
-			default:
-				break;
+		case 0:
+			createPlatform(midpoint);
+			break;
+		case 1:
+			createTrampoline(midpoint);
+			break;
+		default:
+			break;
 		}
 	}
-
+	
 	private int compareGrids(bool[][] a, bool[][] b)
 	{
 		int score = 0;
-
+		
 		for(int i = 0; i < 5; i++)
 		{
 			for(int j = 0; j < 5; j++)
@@ -150,14 +151,14 @@ public class DrawReader : MonoBehaviour
 				score += a[i][j] && b[i][j] ? 1 : 0;
 			}
 		}
-
+		
 		return score;
 	}
-
+	
 	private void createPlatform(Vector2 position)
 	{
 		GameObject platform = GameObject.CreatePrimitive(PrimitiveType.Cube);
-
+		
 		platform.name = "Platform";
 		platform.transform.position = new Vector3(position.x, position.y, 0f);
 		platform.transform.localScale = new Vector3(maxX - minX, 1f, 1f);
@@ -172,50 +173,50 @@ public class DrawReader : MonoBehaviour
 			Destroy(oldPlatform);
 		}
 	}
-
+	
 	private void createTrampoline(Vector2 position)
 	{
 		GameObject trampoline = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-
+		
 		trampoline.name = "Trampoline";
 		trampoline.transform.position = new Vector3(position.x, position.y, 0f);
 		trampoline.transform.localScale = new Vector3(maxX - minX, 0.1f, 1f);
-
+		
 		trampolines.Add(trampoline);
-
+		
 		if(trampolines.Count > maxTrampolines)
 		{
 			GameObject oldTrampoline = trampolines[0];
-
+			
 			trampolines.Remove(oldTrampoline);
 			Destroy(oldTrampoline);
 		}
 	}
-
+	
 	private void setUpPlatformTemplate()
 	{
 		platformTemplate = setUpGrid();
-
+		
 		for(int i = 0; i < 5; i++)
 		{
 			platformTemplate[i][2] = true;
 		}
 	}
-
+	
 	private void setUpTrampolineTemplate()
 	{
 		trampolineTemplate = setUpGrid();
-
+		
 		for(float i = 0f; i < 25f; i++)
 		{
 			trampolineTemplate[2 + (int) (2f * Mathf.Cos(i * 2f * Mathf.PI / 25f))][2 + (int) (2f * Mathf.Sin(i * 2f * Mathf.PI / 25f))] = true;
 		}
 	}
-
+	
 	private bool[][] setUpGrid()
 	{
 		bool[][] grid = {new bool[5], new bool[5], new bool[5], new bool[5], new bool[5]};
-
+		
 		foreach(bool[] row in grid)
 		{
 			for(int i = 0; i < row.Length; i++)
@@ -223,7 +224,7 @@ public class DrawReader : MonoBehaviour
 				row[i] = false;
 			}
 		}
-
+		
 		return grid;
 	}
 }
---
+++ seniorDesign/Assets/Scripts/spawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class spawner : MonoBehaviour {
+	//setup position of where to spawn
+	public float x, y, z;
+	//setup public variable to represent which trigger activated
+	void Start()
+	{
+	}
+	/*void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "playerController") {
+			Debug.Log ("player ENTERS trigger!\n");
+		}
+	}*/
+
+	void OnTriggerStay(Collider other) {
+		if (other.gameObject.name == "playerController") {
+			Debug.Log ("player is WITHIN trigger!\n");
+			other.gameObject.transform.position = new Vector3(x, y, z);
+			other.gameObject.rigidbody.velocity = new Vector3(0, 0, 0);
+		}
+
+	}
+
+}
