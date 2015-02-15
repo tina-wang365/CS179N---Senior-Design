4ab410424613c0e1cbc4f28e81ba72994b00ab7f
mdela011 <mdela011@ucr.edu>
2015-02-15 13:17:32 -0800

    Update PlayerController.cs
    Added edge detection by assuming that there is an invisible wall at every edge

--- seniorDesign/Assets/Scripts/PlayerController.cs
+++ seniorDesign/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 	bool onGround = true; // global variable, bool variable that says if player is on ground or not
 	float moveDirection = 1.0f; // true if the player is moving forward (to the right)
 	float positionBeforeMove = 0.0f;
+	bool atEdge = false; //global variable, bool var to detect when the player is on edge
 	
 	// collision detection between player and various objects
 	void OnCollisionEnter (Collision collider)
@@ -38,7 +39,7 @@ public class PlayerController : MonoBehaviour
 	// update function
 	void FixedUpdate()
 	{
-		float groundHeight = 0.0f;
+		//float groundHeight = 0.0f;
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveSpeed = 0.2f;
 		
@@ -46,28 +47,58 @@ public class PlayerController : MonoBehaviour
 		
 		if(onGround)
 		{
-			// position updated for left/right movement
-			Vector3 movement = new Vector3 (moveHorizontal * moveSpeed, 0.0f, 0.0f);
-			Vector3 newPosition = new Vector3 (transform.position.x + movement.x, transform.position.y, transform.position.z);
-			rigidbody.MovePosition (newPosition);
-			
-			if(positionBeforeMove > newPosition.x && moveDirection == 1.0f)
+			//Added conditoin to check if the player is at the Edge, then you CANNOT move
+			if(atEdge)
 			{
-				moveDirection *= -1;
+				//set player velocity to ZERO
+				rigidbody.velocity = Vector3.zero;
 			}
-			if(positionBeforeMove < newPosition.x && moveDirection == -1.0f)
+
+			//otherwise you are not at the edge and you can left or right
+			else{
+				// position updated for left/right movement
+				Vector3 movement = new Vector3 (moveHorizontal * moveSpeed, 0.0f, 0.0f);
+				Vector3 newPosition = new Vector3 (transform.position.x + movement.x, transform.position.y, transform.position.z);
+				rigidbody.MovePosition (newPosition);
+				
+				if(positionBeforeMove > newPosition.x && moveDirection == 1.0f)
+				{
+					moveDirection *= -1;
+				}
+				if(positionBeforeMove < newPosition.x && moveDirection == -1.0f)
+				{
+					moveDirection *= -1;
+				}
+				//remove this, I believe we dont need it if the player is on the ground
+				//groundHeight = transform.position.y; // gets the height of the ground the player is on
+			}
+
+			if(GameObject.Find ("Platform"))
 			{
-				moveDirection *= -1;
+				//this works to find when a platform is created
+				//Debug.Log("got one");
+				//if there is a platform created Jump to it
+				Jump ();
 			}
-			
-			groundHeight = transform.position.y; // gets the height of the ground the player is on
 		}
 		
 		// program enters character jump function if spacebar is pressed
-		if (Input.GetKey (KeyCode.Space) && transform.position.y == groundHeight && onGround)
+		if (Input.GetKey (KeyCode.Space) && onGround)//transform.position.y == groundHeight && onGround)
 		{
 			Jump();
 			onGround = false; // player is not on ground for duration of the jump
+			atEdge = false;
+		}
+	}
+
+	//OntriggerEnter function to check when the player has reached the edge
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "edge")
+		{
+			atEdge = true;
+			//rigidbody.velocity = Vector3.zero;
+			//Debug.Log ("player is WITHIN EDGE!\n");
 		}
 	}
 }
