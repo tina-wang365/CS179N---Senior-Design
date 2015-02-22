542172e6c7ae402dc471d016ccdd9e68a82c2e1a
twang033 <tina.wang365@gmail.com>
2015-02-21 16:03:56 -0800

    fixed scene reloading and pickup ability.

--- seniorDesign/Assets/Scripts/pickup.cs
+++ seniorDesign/Assets/Scripts/pickup.cs
@@ -3,23 +3,18 @@ using System.Collections;
 
 public class Pickup : MonoBehaviour {
 	public bool levelRestart = false;
-	public bool isActive = true;
+
 	// Use this for initialization
 	void Start () {
-		if (levelRestart) {
-			this.gameObject.SetActive (true); //active must be set to true
-			isActive = true;
-			levelRestart = false;
-		}
+		this.gameObject.SetActive (true);
 
 	}
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.name == "playerController") {
 			this.gameObject.SetActive (false); //active must be set to false
-			isActive = false;
 		}
 	}
-	                                 
+
 	// Update is called once per frame
 	void Update () {
 	
--- seniorDesign/Assets/Scripts/spawner.cs
+++ seniorDesign/Assets/Scripts/spawner.cs
@@ -7,24 +7,19 @@ public class spawner : MonoBehaviour {
 	public float x, y, z;
 	public bool levelEnd = false;
 	public GameObject otherGameObject;
-	private Pickup pickupScript;
 	//setup public variable to represent which trigger activated
 	void Start()
 	{
-		pickupScript = GetComponent<Pickup> ();
-	}
-	/*void OnTriggerEnter(Collider other) {
-		if (other.gameObject.name == "playerController") {
-			Debug.Log ("player ENTERS trigger!\n");
-		}
-	}*/
 
+	}
+	
 	void OnTriggerStay(Collider other) {
 		if (other.gameObject.name == "playerController" && this.gameObject.name == "spikes") {
 			Debug.Log ("Spikes triggered by player!\n");
-			other.gameObject.transform.position = new Vector3 (x, y, z);
-			other.gameObject.rigidbody.velocity = new Vector3 (0, 0, 0);
-			pickupScript.levelRestart = true;
+			//other.gameObject.transform.position = new Vector3 (x, y, z);
+			//other.gameObject.rigidbody.velocity = new Vector3 (0, 0, 0);
+			Application.LoadLevel ("tutorial1");
+
 		} else if (other.gameObject.name == "playerController" && this.gameObject.name == "door") {
 			Debug.Log ("Door triggered by player!\n");
 			Application.LoadLevel ("level1");
