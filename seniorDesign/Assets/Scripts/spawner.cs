bbb5ef53d0d432ecd8916d03ca7dbc353987f656
twang033 <tina.wang365@gmail.com>
2015-02-26 09:44:13 -0800

    made extra prefabs and finished rough scenes

--- seniorDesign/Assets/Scripts/spawner.cs
+++ seniorDesign/Assets/Scripts/spawner.cs
@@ -4,19 +4,20 @@ using System;
 
 public class spawner : MonoBehaviour {
 	//setup position of where to spawn
-	public float x, y, z;
-	public bool levelEnd = false;
-	public GameObject otherGameObject;
+	//public float x, y, z;
+	//public bool levelEnd = false;
 	private string[] levels;
-	private static int level = 0;
+	public static int level = 0;
 	//setup public variable to represent which trigger activated
 	void Start()
 	{
-		levels = new string[] {"tutorial1", "level1", "level2", "level3", "level7"};
+		levels = new string[] {"tutorial1", "level1", "level2", "level3", "level6", "level7", "endScene"};
 	}
 	
 	void OnTriggerStay(Collider other) {
-		if (other.gameObject.name == "playerController" && this.gameObject.name == "spikes") {
+		if (other.gameObject.name == "playerController" && 
+		    (this.gameObject.name == "spikes" || this.gameObject.name == "mediumSpikes3x1"))
+		{
 			Debug.Log ("Spikes triggered by player!\n");
 			//other.gameObject.transform.position = new Vector3 (x, y, z);
 			//other.gameObject.rigidbody.velocity = new Vector3 (0, 0, 0);
