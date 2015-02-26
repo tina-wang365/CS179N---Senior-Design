3d1c9e03233cdd71d1dfa35a308201cad9258c27
twang033 <tina.wang365@gmail.com>
2015-02-26 06:44:29 -0800

    fixed build settings to run scenes. Finished lvl7

--- seniorDesign/Assets/Scripts/spawner.cs
+++ seniorDesign/Assets/Scripts/spawner.cs
@@ -7,27 +7,27 @@ public class spawner : MonoBehaviour {
 	public float x, y, z;
 	public bool levelEnd = false;
 	public GameObject otherGameObject;
-	private Pickup pickupScript;
+	private string[] levels;
+	private static int level = 0;
 	//setup public variable to represent which trigger activated
 	void Start()
 	{
-		pickupScript = GetComponent<Pickup> ();
+		levels = new string[] {"tutorial1", "level1", "level2", "level3", "level7"};
 	}
-	/*void OnTriggerEnter(Collider other) {
-		if (other.gameObject.name == "playerController") {
-			Debug.Log ("player ENTERS trigger!\n");
-		}
-	}*/
-
+	
 	void OnTriggerStay(Collider other) {
 		if (other.gameObject.name == "playerController" && this.gameObject.name == "spikes") {
 			Debug.Log ("Spikes triggered by player!\n");
-			other.gameObject.transform.position = new Vector3 (x, y, z);
-			other.gameObject.rigidbody.velocity = new Vector3 (0, 0, 0);
-			pickupScript.levelRestart = true;
+			//other.gameObject.transform.position = new Vector3 (x, y, z);
+			//other.gameObject.rigidbody.velocity = new Vector3 (0, 0, 0);
+			Debug.Log ("Level = " + level + "\n");
+			Application.LoadLevel (levels[level]);
 		} else if (other.gameObject.name == "playerController" && this.gameObject.name == "door") {
 			Debug.Log ("Door triggered by player!\n");
-			Application.LoadLevel ("level1");
+			Debug.Log ("Level = " + level + "\n");
+			level++;
+			Application.LoadLevel (levels[level]);
+			
 		}
 	}
 }
